e9980e18167169412199e03b5fabaab5746a32b8
maigre <thomas@37m.gr>
2025-05-09 12:59:19 +0200

    install pi2 (non NEON)

--- install.sh
+++ install.sh
@@ -11,7 +11,19 @@ if [[ $(command -v apt) ]]; then
     DISTRO='xbian'
     echo "Distribution: $DISTRO"
 
-    apt install chromium xserver-xorg x11-xserver-utils xinit openbox xdotool
+    apt install xserver-xorg x11-xserver-utils xinit openbox xdotool python3-xdg
+
+    if grep -qw neon /proc/cpuinfo; then
+       echo "NEON is supported"
+       apt install chromium
+    else
+       echo "NEON is not supported"
+       wget https://archive.raspberrypi.org/debian/pool/main/c/chromium-browser/chromium-codecs-ffmpeg-extra_104.0.5112.105-rpt2_armhf.deb
+       wget https://archive.raspberrypi.org/debian/pool/main/c/chromium-browser/chromium-browser_104.0.5112.105-rpt2_armhf.deb
+       apt install ./chromium-codecs-ffmpeg-extra_104.0.5112.105-rpt2_armhf.deb
+       apt install ./chromium-browser_104.0.5112.105-rpt2_armhf.deb
+       ln -s /usr/bin/chromium-browser /usr/bin/chromium
+    fi
 
     # RPi / RP64
     if [[ $(uname -m) = aarch64* || $(uname -m) = armv* ]]; then
